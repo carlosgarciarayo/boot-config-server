9b3e2117d43b0521c42ac85eef71036de4d76e99
carlosgarciarayo201 <carlos@carlos-VirtualBox>
2020-10-20 10:14:21 +0200

    change name proyect

--- src/main/java/com/app/ms/bootconfigserver/BootConfigServerApplication.java
+++ src/main/java/com/app/ms/bootconfigserver/BootConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.app.ms.configserver;
+package com.app.ms.bootconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,10 +7,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class BootConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(BootConfigServerApplication.class, args);
 	}
 
 }
--- src/test/java/com/app/ms/bootconfigserver/BootConfigServerApplicationTests.java
+++ src/test/java/com/app/ms/bootconfigserver/BootConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.app.ms.configserver;
+package com.app.ms.bootconfigserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class BootConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
